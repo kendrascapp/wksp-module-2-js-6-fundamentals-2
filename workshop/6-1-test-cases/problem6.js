6bc51bc9bdbfb9d51d60c279224fafcdd7ca32f3
Kendra Scappaticci <k.scapp@gmail.com>
2020-03-03 12:27:53 -0500

    done

--- workshop/6-1-test-cases/problem6.js
+++ workshop/6-1-test-cases/problem6.js
@@ -21,13 +21,13 @@ let verifyEquals = require('../../assets/verify-equals');
 function f(arr) {
   const operation = arr[0];
   switch (operation) {
-      case 'add':
-          return arr.slice(1).reduce((prev, next) => prev + next);
-      case 'sub':
-          return arr.slice(1).reduce((prev, next) => prev - next);
-      case 'mult':
-          return arr.slice(1).reduce((prev, next) => prev * next);
-      default:
+    case 'add':
+      return arr.slice(1).reduce((prev, next) => prev + next);
+    case 'sub':
+      return arr.slice(1).reduce((prev, next) => prev - next);
+    case 'mult':
+      return arr.slice(1).reduce((prev, next) => prev * next);
+    default:
       return undefined;
   }
 }
